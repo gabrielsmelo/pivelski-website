10d89d46a48aef69a68a3e4eac650db53e203dd1
Gabriel Soares <gabrielsmelo.dev@gmail.com>
2025-05-09 12:31:08 +0200

    feat: adding responsivity and fixing the videos

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
-import chivas from './assets/chivas.png';
-import shein from './assets/shein.png';
-import growth from './assets/growth.png';
-import bubbaloo from './assets/bubbaloo.png';
-import fashionNova from './assets/fashionNova.png';
-import mcd from './assets/mcd.png';
-import lost from './assets/lost.png';
-import mercadolivre from './assets/mercadolivre.png';
+import chivas from './assets/logos/chivas.png';
+import shein from './assets/logos/shein.png';
+import growth from './assets/logos/growth.png';
+import bubbaloo from './assets/logos/bubbaloo.png';
+import fashionNova from './assets/logos/fashionNova.png';
+import mcd from './assets/logos/mcd.png';
+import lost from './assets/logos/lost.png';
+import mercadolivre from './assets/logos/mercadolivre.png';
+import instagram from './assets/logos/instagram.svg';
+import youtube from './assets/logos/youtube.svg';
+
+// Import videos
+const widescreen1 = '/assets/videos/caos.mp4';
+const widescreen2 = '/assets/videos/brands-e-arthur.mp4';
+const widescreen3 = '/assets/videos/alee.mp4';
+const square1 = '/assets/videos/globoplay.mp4';
+const square2 = '/assets/videos/tiago-gomez.mp4';
 
 const work = [
   { src: shein, alt: 'Shein' },
@@ -19,15 +28,26 @@ const work = [
   { src: lost, alt: 'Lost Brasil', width: '210'},
 ];
 
+const videos = {
+  widescreen: [
+    { src: widescreen1, alt: 'Caos' },
+    { src: widescreen2, alt: 'Brands & Arthur' },
+    { src: widescreen3, alt: 'ALEE' },
+  ],
+  square: [
+    { src: square1, alt: 'Globoplay' },
+    { src: square2, alt: 'Tiago Gomez' },
+  ]
+}
+
 export default function Home() {
-  const pageUrl = typeof window !== 'undefined' ? window.location.href : 'http://localhost:3001';
   return (
     <div id="main" className="grid items-center justify-items-center min-h-screen p-10 pb-20 pt-20">
       <nav className="mainNav"><a href="#main">HOME</a></nav>
       <main className="flex flex-col row-start-2 items-center sm:items-start">
-        <div>
+        <div className="mainBlock">
           <video className="mainVideo rounded-3xl" autoPlay muted loop preload="true">
-            <source src="/videos/brands-arthur.mp4" type="video/mp4" />
+            <source src={videos.widescreen[0].src} alt={videos.widescreen[0].alt} type="video/mp4" />
             {"Your browser doesn't support this website."}
           </video>
           <div className="mainTitle">
@@ -41,20 +61,20 @@ export default function Home() {
           <div className="spacerWrap"><div className="spacerIcon"></div><div className="spacerLine"></div><div className="spacerIcon rec"></div><div className="spacerLine"></div><div className="spacerIcon"></div></div>
         </div>
 
-        <div className="pt-36 pb-36 companyTitle text-center sm:text-left">
+        <div className="pt-36 pb-36 bio text-center sm:text-left">
           <h2>Handerson Santana, também conhecido como @Pivelski, é um artista e cineasta que combina seu interesse por cinema, cultura e lugares por meio de peças narrativas curtas e longas.</h2>
         </div>
 
-        <div className="section flex items-center justify-center self-center">
+        <div className="section flex items-center justify-center self-center squareSection">
           <video className="container rounded-3xl" autoPlay muted loop preload="true">
-            <source src="/videos/square-1.mp4" type="video/mp4"></source>
+            <source src={videos.square[0].src} alt={videos.square[0].alt} type="video/mp4"></source>
           </video>
           <div className="container flex flex-col justify-center rounded-3xl mr-5 ml-5 border-solid border border-gray-900">
             <h3 className="font-bold uppercase text-6xl text-center self-center">The story <br /> always <br /> comes first</h3>
             <p className="text-center self-center mt-5 font-bold uppercase text-neutral-500 text-xs">Pivelski films production</p>
           </div>
           <video className="container rounded-3xl" autoPlay muted loop preload="true">
-            <source src="https://videos.pexels.com/video-files/8027440/8027440-uhd_2732_1440_25fps.mp4" type="video/mp4"></source>
+            <source src={videos.square[1].src} alt={videos.square[1].alt} type="video/mp4"></source>
           </video>
         </div>
 
@@ -96,28 +116,32 @@ export default function Home() {
         <div className="section p-36 min-w-full max-w-full">
           <h2 className="uppercase font-bold originTitle">DIRETOR CRIATIVO NASCIDO NA BAHIA</h2>
           <video className="rounded-3xl min-w-full mt-20 originVideo" autoPlay muted loop preload="true">
-            <source src="/videos/brands-arthur.mp4" type="video/mp4"></source>
+            <source src={videos.widescreen[1].src} alt={videos.widescreen[1].alt} type="video/mp4"></source>
           </video>
         </div>
 
         <div className="section flex items-center self-center flex-col min-w-full max-w-full justify-center">
-          <h3 className="font-bold text-5xl">WORK</h3>
-          <div className="grid grid-rows-2 grid-cols-4 mt-6 rounded-3xl border-solid border border-gray-900 min-w-full">
+          <h3 className="font-bold text-5xl workTitle">WORK</h3>
+          <div className="companyGrid grid grid-rows-2 grid-cols-4 mt-6 rounded-3xl border-solid border border-gray-900 min-w-full">
             {work.map(company => <div key={company.alt} className="company flex justify-center"><Image className="self-center m-10" key={company.alt} src={company.src} width={company.width || '136'} alt={company.alt} /></div> )}
           </div>
         </div>
 
         <div className="relative">
           <video className="lastVideo mt-36 rounded-3xl" autoPlay muted loop preload="true">
-            <source src="/videos/praiano-beach.mp4" type="video/mp4" />
+            <source src={videos.widescreen[2].src} alt={videos.widescreen[2].alt} type="video/mp4" />
             {"Your browser doesn't support this website."}
           </video>
           <div className="absolute flex flex-col justify-center items-center inset-x-0 inset-y-0 mt-36">
             <p className="uppercase tracking-widest text-s font-bold mb-10">pivelski@gmail.com</p>
-            <h2 className="font-bold uppercase text-9xl">Get in touch</h2>
+            <h2 className="getInTouch font-bold uppercase text-9xl">Get in touch</h2>
             <div className="flex items-center mt-10">
-              <a className="contactButton" href="https://instagram.com/pivelski">IN</a>
-              <a className="contactButton" href="https://youtube.com">YT</a>
+              <a className="contactButton" href="https://instagram.com/pivelski" target="_blank">
+                <Image src={instagram} className="invert" alt="Instagram" />
+              </a>
+              <a className="contactButton" href="https://youtube.com" target="_blank">
+                <Image src={youtube} className="invert" alt="Youtube" />
+              </a>
             </div>
           </div>
         </div>
