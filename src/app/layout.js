8a42aafc68a4837550f186c64d6d491d73e83435
Gabriel Soares <gabrielsmelo.dev@gmail.com>
2025-06-04 23:06:53 +0200

    fix: add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
-import { Red_Hat_Display }from "next/font/google";
+import { Red_Hat_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const redHatDisplay = Red_Hat_Display({
   variable: "--font-red-hat",
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -14,10 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${redHatDisplay.variable} antialiased`}
-      >
+      <body className={`${redHatDisplay.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
